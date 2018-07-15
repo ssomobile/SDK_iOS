242ce6cff430c25aa6f4b07b92078631e97700c5
Anton Sleptsov <>
2018-07-15 21:05:58 +0300

    Version 0.0.1

---
+++ SSOMobile.framework/Headers/SSOMobile.h
@@ -0,0 +1,19 @@
+//
+//  sso_ios_sdk.h
+//  sso_ios_sdk
+//
+//  Created by Anton Sleptsov on 14/06/2018.
+//  Copyright © 2018 Anton Sleptsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sso_ios_sdk.
+FOUNDATION_EXPORT double sso_ios_sdkVersionNumber;
+
+//! Project version string for sso_ios_sdk.
+FOUNDATION_EXPORT const unsigned char sso_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sso_ios_sdk/PublicHeader.h>
+
+
